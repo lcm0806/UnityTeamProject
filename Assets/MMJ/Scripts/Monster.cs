c2dd9656c17e6494ab28e06cbef0759a9484f44f
mjmin99 <mjmin99@naver.com>
2025-04-28 15:57:41 +0900

    [Feat] 몬스터 구현
    몬스터의 체력과 피격, 일정조건하에 따른 사망, 피격과, 사망시 색 변경등을 구현했다.

--- Assets/MMJ/Scripts/Monster.cs
+++ Assets/MMJ/Scripts/Monster.cs
@@ -1,32 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Monster : MonoBehaviour
 {
-    [SerializeField] int hp;
-    [SerializeField] int attackDamage;
-    [SerializeField] float speed;
-    [SerializeField] LayerMask targetLayer;
-    [SerializeField] Transform targetPos;
-    [SerializeField] Transform MuzzleRos;
+    public int maxHealth;
+    public int curHealth;
 
-    private void DetectPlayer()
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    Material mat;
+
+
+    private void Awake()
     {
-        //todo
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        mat = GetComponentInChildren<MeshRenderer>().material;
     }
 
-    private void Move()
+    private void OnTriggerEnter(Collider other)
     {
-        //todo
+        if (other.tag == "공격수단") //todo
+        {
+            //공격수단(태그지정) 공격수단이름 = other.GetComponent<공격수단>();
+            //curHealth -= 공격수단공격력;
+            Vector3 reactVec = transform.position - other.transform.position;
+
+            //Destroy(other.gameObject); //(총알 같은 투사체일경우 맞았을때 삭제)
+
+            StartCoroutine(OnDamage(reactVec));
+        }
     }
 
-    private void Attack()
-    {
-        //todo
-    }
-    private void Die()
-    {
-        //todo
+    IEnumerator OnDamage(Vector3 reactVec)
+    { 
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if (curHealth > 0)
+        {
+            mat.color = Color.white;
+        }
+        else
+        {
+            mat.color = Color.gray;
+            gameObject.layer = 21;
+
+            reactVec = reactVec.normalized;
+            reactVec += Vector3.up;
+            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+            Destroy(gameObject, 4);
+        }
     }
 }
