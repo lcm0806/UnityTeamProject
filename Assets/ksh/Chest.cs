04eeb437161df04d6f41c8a912b4daffe3fd3d20
76sohyun <conut1119@gmail.com>
2025-04-28 13:03:01 +0900

    [Feat] 아이템 랜덤으로 나오는 상자 구현중

---
+++ Assets/ksh/Chest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   public List<Item> items = new List<Item>();
+    public Transform ItemSpawn;
+    private float ThrowForce = 5f;
+
+    public void OpenChest()
+    {
+        
+        Rigidbody rb = item.GetComponent<Rigidbody>();
+        
+        if(rb != null)
+        {
+            rb.AddForce(Vector3.up * ThrowForce, ForceMode.Impulse);
+        }
+        
+    }
+}
