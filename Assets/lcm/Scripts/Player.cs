8a4bea1342096035890c258de5f2b97f5ab094fc
lcm0806 <82375404+lcm0806@users.noreply.github.com>
2025-04-29 09:42:20 +0900

    Revert "[Feat] 플레이어 기본스탯 변수추가"
    This reverts commit 821f2dd28c2f656e04816a918816e58b4bc08595.

--- Assets/lcm/Scripts/Player.cs
+++ Assets/lcm/Scripts/Player.cs
@@ -11,12 +11,6 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpPower;
     [SerializeField] private int health;
     [SerializeField] private int Damage;
-    [SerializeField] private int coin;
-    [SerializeField] private int hasGrenades;
-
-    [SerializeField] private int maxHasGrenades;
-    [SerializeField] private int maxHealth;
-    [SerializeField] private int maxCoin;
 
     private bool wDown;
     private bool jDown;
