efdb92117c7ea41b56c207de6327f73aaa0ef993
mjmin99 <mjmin99@naver.com>
2025-04-29 10:49:11 +0900

    [Feat] 몬스터의 플레이어 피격 구현
    플레이어 피격을 구현함과 애니메이션 적용

--- Assets/MMJ/Scripts/Monster.cs
+++ Assets/MMJ/Scripts/Monster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Monster : MonoBehaviour
 {
@@ -9,7 +10,9 @@ public class Monster : MonoBehaviour
     public int curHealth;
     public Transform target;
     public float moveSpeed;
+    public BoxCollider meleeArea;
     public bool isChase;
+    public bool isAttack;
 
 
     Rigidbody rigid;
@@ -47,12 +50,47 @@ public class Monster : MonoBehaviour
             anime.SetBool("isWalk", true);
             Trace();
         }
-        else
-        {
-            anime.SetBool("isWalk", false);
+      //  else
+      //  {
+      //      anime.SetBool("isWalk", false);
+      //  }
+    }
+
+    private void FixedUpdate()
+    {
+        Targeting();
+    }
+
+    void Targeting()
+    {
+        float targetRadius = 1.5f;
+        float targetRange = 3f;
+
+        RaycastHit[] rayHits = Physics.SphereCastAll(transform.position, targetRadius, transform.forward, targetRange, LayerMask.GetMask("Player"));
+
+        if (rayHits.Length > 0 && !isAttack)
+        { 
+            StartCoroutine(Attack());
         }
     }
 
+    IEnumerator Attack()
+    {
+        isChase = false;
+        isAttack = true;
+        anime.SetBool("isAttack", true);
+
+        yield return new WaitForSeconds(0.2f);
+        meleeArea.enabled = true;
+
+        yield return new WaitForSeconds(1f);
+        meleeArea.enabled = false;
+
+        yield return new WaitForSeconds(1f);
+        isChase = true;
+        isAttack = false;
+        anime.SetBool("isAttack", false);
+    }
 
     IEnumerator OnDamage(Vector3 reactVec)
     { 
