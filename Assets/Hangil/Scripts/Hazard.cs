c38b08e3b3dc485f73ca0f1bb6ae299a11ef6982
Hangil Chang <hangil2010@naver.com>
2025-04-29 14:49:39 +0900

    [Feat] 적을 모두 처치할 경우 이동 가능하게 하는 기능 추가
    모든 적군을 처치할 경우 문이 비활성화되고 맵 이동 포인트가 활성화되게 함

--- Assets/Hangil/Scripts/Hazard.cs
+++ Assets/Hangil/Scripts/Hazard.cs
@@ -5,22 +5,29 @@ using UnityEngine.Events;
 
 public class Hazard : MonoBehaviour
 {
-    public Transform teleportPos;
+    //public Transform teleportPos;
+    public Transform[] teleportPosList;
     public float teleportYPosPadding = 5.5f;
     [Header("함정에 닿았을 때 발동할 효과들 지정")]
     public UnityEvent OnHazardFall;
     // TODO : 빠질 경우 이펙트 추가하기?
-
-    
+    private int posListCount;
+    private void Start()
+    {
+        posListCount = teleportPosList.Length;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (teleportPos != null)
+            if (teleportPosList != null)
             {
                 OnHazardFall?.Invoke();
-                other.transform.position = new Vector3(teleportPos.position.x, teleportPos.position.y + teleportYPosPadding, teleportPos.position.z);
+                int teleRandNum = Random.Range(0, posListCount-1);
+                other.transform.position = new Vector3(teleportPosList[teleRandNum].position.x, 
+                                                       teleportPosList[teleRandNum].position.y + teleportYPosPadding,
+                                                       teleportPosList[teleRandNum].position.z);
             }
         }
     }
