b17c2142c644a39f11ed06311ac4ea0777d9b436
Hangil Chang <hangil2010@naver.com>
2025-05-02 12:05:47 +0900

    [Style] CameraManager > MapManager로 이름 변경

--- Assets/Hangil/Scripts/MapManager.cs
+++ Assets/Hangil/Scripts/MapManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+public class MapManager : MonoBehaviour
 {
-    public static CameraManager instance;
+    public static MapManager instance;
     public static int currentMap;
     [SerializeField] Camera mainCamera;
     public List<GameObject> Maps;
